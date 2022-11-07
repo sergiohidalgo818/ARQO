3f3b31622945ac4f889490f4b23d335ffd819380
Sergio <sergiohg818@gmail.com>
2022-11-07 17:49:06 +0100

    upload

--- p3/Ejercicio1/Ejercicio1.bash
+++ p3/Ejercicio1/Ejercicio1.bash
@@ -8,11 +8,12 @@ let N1=1024
 for (( j=1; j<=10; j++ ))
 do
 	
-	(echo N$j.$i $N1 slow && ../material_P3/./slow $N1 | grep time && echo fast && ../material_P3/./fast $N1 | grep time) | echo $(cat $1) >> Ejercicio1-1.txt
+	(echo N$j.$i $N1 slow && ../material_P3/./slow $N1 | grep time && echo fast && ../material_P3/./fast $N1 | grep time) | echo $(cat $1) >> time_slow_fast.dat
 	N1=$((1024 + N1))
 done
 
 
 done
 
+awk '{print $2, $6, $10}' time_slow_fast.dat > time_slow_fast.dat
 
